408c4a649e8f140b2e940de28fa288cc201b614c
Mayara Gandolfi <112402452+MayaraGandolfi@users.noreply.github.com>
2022-10-03 16:15:03 -0300

    Inserindo função de verificação do vencedor
    Inserindo função de verificação do vencedor

--- src/br/com/jogodavelha/JogoDaVelha.java
+++ src/br/com/jogodavelha/JogoDaVelha.java
@@ -23,11 +23,11 @@ public class JogoDaVelha {
         while (continuaJogando == 1) {  //loop para jogar novamente
 
             int jogadas = 1;
+            int vencedor = 0;
             inciaTabuleiro(tabuleiro);
+            mostraTabuleiro(tabuleiro);
 
-            while (jogadas <= 9) {
-                mostraTabuleiro(tabuleiro);
-
+            while (vencedor == 0 && jogadas <= 9) {
                 char simbolo;
                 if (numImpar(jogadas)) {
                     simbolo = 'X';
@@ -71,19 +71,23 @@ public class JogoDaVelha {
                     default:
                         break;
                 }
+                mostraTabuleiro(tabuleiro);
                 if (jogadas >= 5) {
-                    //verifica se alguem ja ganhou
-                    //se alguem já ganhou soma a vitoria na variavel, sai do loop e pergunta se que jogar de novo
+                    vencedor = verificaVencedor(tabuleiro);
+                    if (vencedor == 1) {
+                        System.out.printf("Parabéns, %s!!!", jogador1);
+                        vitoriasJogador1++;
+                    } else if (vencedor == 2) {
+                        System.out.printf("Parabéns, %s!!!", jogador2);
+                        vitoriasJogador2++;
+                    }
                 }
-                if (jogadas == 9) {
-                    //deu empate
-                    //sai do loop e pergunta se quer jogar de novo
-
+                if (jogadas == 9 && vencedor == 0) {
+                    System.out.println("O resultado foi empate!");
                 }
                 jogadas++;
             }
-            System.out.println("Fim da partida\n");
-            mostraTabuleiro(tabuleiro);
+            System.out.println("\n\nFim da partida.\n");
             System.out.println("Deseja jogar novamente ? - Digite 1 para continuar ou 0 para encerrar o jogo");
             try {
                 continuaJogando = scanner.nextInt();
@@ -98,11 +102,10 @@ public class JogoDaVelha {
         }
 
         System.out.println("\n** Fim de jogo **\n");
-        mostraTabuleiro(tabuleiro);
+        //mostraTabuleiro(tabuleiro);
         //mostra o placar final
 
         scanner.close();
-
     }
 
     public static void mostraTabuleiro(char[][] tabuleiro) {
@@ -130,5 +133,34 @@ public class JogoDaVelha {
 
     public static boolean numImpar(int num) {
         return num % 2 == 1;
+    }
+
+    public static int verificaVencedor(char[][] tabuleiro) {
+
+        String linha1 = "" + tabuleiro[0][0] + tabuleiro[0][1] + tabuleiro[0][2];
+        String linha2 = "" + tabuleiro[1][0] + tabuleiro[1][1] + tabuleiro[1][2];
+        String linha3 = "" + tabuleiro[2][0] + tabuleiro[2][1] + tabuleiro[2][2];
+
+        String coluna1 = "" + tabuleiro[0][0] + tabuleiro[1][0] + tabuleiro[2][0];
+        String coluna2 = "" + tabuleiro[0][1] + tabuleiro[1][1] + tabuleiro[2][1];
+        String coluna3 = "" + tabuleiro[0][2] + tabuleiro[1][2] + tabuleiro[2][2];
+
+        String diagonal1 = "" + tabuleiro[0][0] + tabuleiro[1][1] + tabuleiro[2][2];
+        String diagonal2 = "" + tabuleiro[0][2] + tabuleiro[1][1] + tabuleiro[2][0];
+
+        if (linha1.equals("XXX") || linha2.equals("XXX") || linha3.equals("XXX") ||
+                coluna1.equals("XXX") || coluna2.equals("XXX") || coluna3.equals("XXX") ||
+                diagonal1.equals("XXX") || diagonal2.equals("XXX")) {
+            System.out.println("\nX VENCEU A RODADA");
+            return 1;
+        }
+
+        if (linha1.equals("OOO") || linha2.equals("OOO") || linha3.equals("OOO") ||
+                coluna1.equals("OOO") || coluna2.equals("OOO") || coluna3.equals("OOO") ||
+                diagonal1.equals("OOO") || diagonal2.equals("OOO")) {
+            System.out.println("\nO VENCEU A RODADA");
+            return 2;
+        }
+        return 0;
     }
 }
