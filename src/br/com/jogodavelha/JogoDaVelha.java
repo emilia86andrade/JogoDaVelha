ac750165028e6913da3e7f86bf395f653af81ade
Mayara Gandolfi <112402452+MayaraGandolfi@users.noreply.github.com>
2022-10-05 10:49:56 -0300

    Ajustando verificação da jogada
    Ajustando verificação da jogada

--- src/br/com/jogodavelha/JogoDaVelha.java
+++ src/br/com/jogodavelha/JogoDaVelha.java
@@ -21,147 +21,149 @@ public class JogoDaVelha {
         int vitoriasJogador1 = 0;
         int vitoriasJogador2 = 0;
         char[][] tabuleiro = new char[3][3];
-        boolean testeDeJogada1 = true;
-        boolean testeDeJogada2 =true;
-        boolean testeDeJogada3 = true;
-        boolean testeDeJogada4 =true;
-        boolean testeDeJogada5 = true;
-        boolean testeDeJogada6 =true;
-        boolean testeDeJogada7 = true;
-        boolean testeDeJogada8 =true;
-        boolean testeDeJogada9 =true;
-        int continuaJogando = 1;
 
+        int continuaJogando = 1;
         //loop para jogar novamente
         while (continuaJogando == 1) {
+            boolean testeDeJogada1 = true;
+            boolean testeDeJogada2 = true;
+            boolean testeDeJogada3 = true;
+            boolean testeDeJogada4 = true;
+            boolean testeDeJogada5 = true;
+            boolean testeDeJogada6 = true;
+            boolean testeDeJogada7 = true;
+            boolean testeDeJogada8 = true;
+            boolean testeDeJogada9 = true;
             int jogadas = 1;
             int vencedor = 0;
             inciaTabuleiro(tabuleiro);
-            mostraTabuleiro(tabuleiro);
             while (vencedor == 0 && jogadas <= 9) {
                 char simbolo;
-                if (numImpar(jogadas)) {
-                    simbolo = 'X';
-                    System.out.println("\nJogador " + jogador1 + " escolha uma posição de 1 a 9 para marcar o X: ");
-                } else {
-                    simbolo = 'O';
-                    System.out.println("\nJogador " + jogador2 + " escolha uma posição de 1 a 9 para marcar o O: ");
-                }
-                char jogada = scanner.next().charAt(0);
+                int erroJogada;
+                do {
+                    mostraTabuleiro(tabuleiro);
+                    erroJogada = 0;
+                    if (numImpar(jogadas)) {
+                        simbolo = 'X';
+                        System.out.println("\nJogador " + jogador1 + " escolha uma posição de 1 a 9 para marcar o X: ");
+                    } else {
+                        simbolo = 'O';
+                        System.out.println("\nJogador " + jogador2 + " escolha uma posição de 1 a 9 para marcar o O: ");
+                    }
+                    char jogada = scanner.next().charAt(0);
 
-                //foi criado um booleano para verificar se a posição já foi selecionada
-                switch (jogada) {
-                    case '1':
-                        tabuleiro[0][0] = simbolo;
-                        if( testeDeJogada1 ==true){testeDeJogada1 =false;
+                    //foi criado um booleano para verificar se a posição já foi selecionada
+                    switch (jogada) {
+                        case '1':
+                            if (testeDeJogada1 == true) {
+                                testeDeJogada1 = false;
+                                tabuleiro[0][0] = simbolo;
+                            } else {
+                                erroJogada = 1;
+                                System.out.println("Essa opção já foi escolhida.");
+                            }
+                            break;
+                        case '2':
+                            if (testeDeJogada2 == true) {
+                                testeDeJogada2 = false;
+                                tabuleiro[0][1] = simbolo;
+                            } else {
+                                erroJogada = 1;
+                                System.out.println("Essa opção já foi escolhida.");
+                            }
+                            break;
+                        case '3':
+                            if (testeDeJogada3 == true) {
+                                testeDeJogada3 = false;
+                                tabuleiro[0][2] = simbolo;
+                            } else {
+                                erroJogada = 1;
+                                System.out.println("Essa opção já foi escolhida.");
+                            }
+                            break;
+                        case '4':
+                            if (testeDeJogada4 == true) {
+                                testeDeJogada4 = false;
+                                tabuleiro[1][0] = simbolo;
+                            } else {
+                                erroJogada = 1;
+                                System.out.println("Essa opção já foi escolhida.");
+                            }
+                            break;
+                        case '5':
+                            if (testeDeJogada5 == true) {
+                                testeDeJogada5 = false;
+                                tabuleiro[1][1] = simbolo;
+                            } else {
+                                System.out.println("Essa opção já foi escolhida.");
+                                erroJogada = 1;
+                            }
+                            break;
+                        case '6':
+                            if (testeDeJogada6 == true) {
+                                testeDeJogada6 = false;
+                                tabuleiro[1][2] = simbolo;
+                            } else {
+                                erroJogada = 1;
+                                System.out.println("Essa opção já foi escolhida.");
+                            }
+                            break;
+                        case '7':
+                            if (testeDeJogada7 == true) {
+                                testeDeJogada7 = false;
+                                tabuleiro[2][0] = simbolo;
+                            } else {
+                                erroJogada = 1;
+                                System.out.println("Essa opção já foi escolhida.");
+                            }
+                            break;
+                        case '8':
+                            if (testeDeJogada8 == true) {
+                                testeDeJogada8 = false;
+                                tabuleiro[2][1] = simbolo;
+                            } else {
+                                erroJogada = 1;
+                                System.out.println("Essa opção já foi escolhida.");
+                            }
+                            break;
+                        case '9':
+                            if (testeDeJogada9 == true) {
+                                testeDeJogada9 = false;
+                                tabuleiro[2][2] = simbolo;
+                            } else {
+                                erroJogada = 1;
+                                System.out.println("Essa opção já foi escolhida.");
+                            }
+                            break;
+                        default:
+                            System.out.println("Opção inválida, é necessário digitar de 1 a 9!");
+                            erroJogada = 1;
+                    }
+                } while (erroJogada == 1);
 
-                        }else{
-                            System.out.println("Jogo Encerrado. Essa opção já foi escolhida.");
-                            return;
-                     }
-                        tabuleiro[0][0] = simbolo;
-                        break;
-
-                    case '2':
-                        if(testeDeJogada2 ==true){
-                            testeDeJogada2 =false;
-                        }else {
-
-                            System.out.println("Jogo Encerrado. Essa opção já foi escolhida.");
-                            return;
-                       }
-                        tabuleiro[0][1] = simbolo;
-                        break;
-                    case '3':
-                        if(testeDeJogada3 ==true){testeDeJogada3 =false;
-                        }else{
-
-                            System.out.println("Jogo Encerrado. Essa opção já foi escolhida.");
-                            return;
-                        }
-                        tabuleiro[0][2] = simbolo;
-                        break;
-                    case '4':
-                        if(testeDeJogada4 ==true){testeDeJogada4 =false;
-                        }else{
-
-                            System.out.println("Jogo Encerrado. Essa opção já foi escolhida.");
-                            return;
-                        }
-                        tabuleiro[1][0] = simbolo;
-                        break;
-                    case '5':
-                        if(testeDeJogada5 ==true){testeDeJogada5 =false;
-                        }else{
-                            System.out.println("Jogo Encerrado. Essa opção já foi escolhida.");
-                            return;
-                        }
-                        tabuleiro[1][1] = simbolo;
-                        break;
-                    case '6':
-                        if(testeDeJogada6 ==true){testeDeJogada6 =false;
-                        }else{
-
-                            System.out.println("Jogo Encerrado. Essa opção já foi escolhida.");
-                            return;
-                        }
-                        tabuleiro[1][2] = simbolo;
-                        break;
-                    case '7':
-                        if(testeDeJogada7 ==true){testeDeJogada7 =false;
-                        }else{
-
-                            System.out.println("Jogo Encerrado. Essa opção já foi escolhida.");
-                            return;
-                        }
-                        tabuleiro[2][0] = simbolo;
-                        break;
-                    case '8':
-                        if(testeDeJogada8 ==true){testeDeJogada8 =false;
-                        }else{
-                            System.out.println("Jogo Encerrado. Essa opção já foi escolhida.");
-                            return;
-                        }
-                        tabuleiro[2][1] = simbolo;
-                        break;
-                    case '9':
-                        if(testeDeJogada9 ==true){testeDeJogada9 =false;
-                        }else{
-                            System.out.println("Jogo Encerrado. Essa opção já foi escolhida.");
-                            return;
-
-                        }
-                        tabuleiro[2][2] = simbolo;
-                        break;
-                    default:
-                        System.out.println("Opção inválida, o jogo foi encerrado pois precisa digitar de 1 a 9!");
-                   return;
-                }
-
-                mostraTabuleiro(tabuleiro);
-
-                //condional para verificar vencedor
+                //condicional para verificar vencedor
                 if (jogadas >= 5) {
                     vencedor = verificaVencedor(tabuleiro);
                     if (vencedor == 1) {
-                        System.out.printf("Parabéns, %s!!!", jogador1);
+                        System.out.printf("Parabéns, %s!!!\n", jogador1);
                         vitoriasJogador1++;
                     } else if (vencedor == 2) {
-                        System.out.printf("Parabéns, %s!!!", jogador2);
+                        System.out.printf("Parabéns, %s!!!\n", jogador2);
                         vitoriasJogador2++;
                     }
                 }
 
                 //condicional para verificar empate
                 if (jogadas == 9 && vencedor == 0) {
-                    System.out.println("O resultado foi empate!");
+                    System.out.println("O resultado foi empate!\n");
                 }
                 jogadas++;
             }
 
-            System.out.println("\n\nFim da partida.\n");
-            System.out.println("Deseja jogar novamente ? - Digite 1 para continuar ou 0 para encerrar o jogo");
+            mostraTabuleiro(tabuleiro);
 
+            System.out.println("\nFim da partida.\n");
+            System.out.println("Deseja jogar novamente ? - Digite 1 para continuar ou 0 para encerrar o jogo");
             try {
                 continuaJogando = scanner.nextInt();
                 if (continuaJogando != 0 && continuaJogando != 1) {
@@ -173,13 +175,24 @@ public class JogoDaVelha {
                 continuaJogando = 0;
             }
         }
+        System.out.println();
 
-        System.out.println("\n** Fim de jogo **\n");
-        //mostraTabuleiro(tabuleiro);
-        //mostra o placar final
+        //placar final
+        System.out.println("** Fim de jogo **\n");
+        System.out.println("****** PLACAR FINAL ******");
+        System.out.printf("Jogador X %s - %d vitórias\n", jogador1, vitoriasJogador1);
+        System.out.printf("Jogador O %s - %d vitórias\n", jogador2, vitoriasJogador2);
+        if (vitoriasJogador1 > vitoriasJogador2) {
+            System.out.printf("\nParabéns %s, você venceu!", jogador1);
+        } else if (vitoriasJogador1 < vitoriasJogador2) {
+            System.out.printf("\nParabéns %s, você venceu!", jogador2);
+        } else {
+            System.out.println("\nO resultado deu empate!");
+        }
 
         scanner.close();
     }
+
     //função para desenhar tabuleiro
     public static void mostraTabuleiro(char[][] tabuleiro) {
 
@@ -190,6 +203,7 @@ public class JogoDaVelha {
         System.out.println(tabuleiro[2][0] + " | " + tabuleiro[2][1] + " | " + tabuleiro[2][2]);
 
     }
+
     //função para iniciar tabuleiro atribuindo valores numéricos para a matriz
     public static void inciaTabuleiro(char[][] tabuleiro) {
         //talvez por looping - decobrir como converte int pra char
@@ -203,6 +217,7 @@ public class JogoDaVelha {
         tabuleiro[2][1] = '8';
         tabuleiro[2][2] = '9';
     }
+
     //função para veirificar se é o jogador X ou O
     public static boolean numImpar(int num) {
         return num % 2 == 1;
